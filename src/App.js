bf92843b07389fe84944376471706a1617064e12
farnaz <farnaz.towhidi@gmail.com>
2024-12-04 20:51:01 -0500

    Update DELETE

--- src/App.js
+++ src/App.js
@@ -8,56 +8,48 @@ function App() {
   
   const InitialProducts = [
     {
-    id: 1,
     name: "Laptop",
     price: 999.99,
     category: "Electronics",
     featured: false // New field added
     },
     {
-    id: 2,
     name: "T-shirt",
     price: 19.99,
     category: "Clothing",
     featured: true // New field added
     },
     {
-    id: 3,
     name: "Microwave",
     price: 89.99,
     category: "Home Essentials",
     featured: false // New field added
     },
     {
-    id: 4,
     name: "Smartphone",
     price: 699.99,
     category: "Electronics",
     featured: true // New field added
     },
     {
-    id: 5,
     name: "Jeans",
     price: 39.99,
     category: "Clothing",
     featured: false // New field added
     },
     {
-    id: 6,
     name: "Blender",
     price: 49.99,
     category: "Home Essentials",
     featured: false // New field added
     },
     {
-    id: 7,
     name: "Wireless Headphones",
     price: 159.99,
     category: "Electronics",
     featured: false // New field added
     },
     {
-    id: 8,
     name: "Socks",
     price: 5.99,
     category: "Clothing",
--- src/component/ProductList/ProductList.jsx
+++ src/component/ProductList/ProductList.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
 import Table from 'react-bootstrap/Table';
+import Button from 'react-bootstrap/Button';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function ProductList({products,setProducts}) {
+
+  let handleDelete = (event, pindex, pname) => {
+    console.log ("DELETE")
+    console.log (event)
+    console.log (pindex)
+    console.log (pname)
+
+    let newProductList = products.filter ((product,index)=>index!==pindex)
+    setProducts (newProductList)
+  }
+
+
   return (
     <>
       <Table striped bordered hover>
@@ -13,17 +27,19 @@ export default function ProductList({products,setProducts}) {
           <th>Product Price</th>
           <th>Product Category</th>
           <th>Product Featured</th>
+          <th>Action</th>
         </tr>
       </thead>
       <tbody>
         {
           products.map ((product, index)=>(
             <tr key={index}>
-            <td>1</td>
+            <td>{index}</td>
             <td>{product.name}</td>
             <td>{product.price}</td>
             <td>{product.category}</td>
             <td>{product.featured ? "✦" : "✎"}</td>
+            <td><Button variant="danger" onClick={(event)=>handleDelete(event,index, "product.name")}>DELETE</Button></td>
           </tr>
           ))
         }
